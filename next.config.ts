8bae6a5c622b53a2319e0dc4fb55acfb0374e22d
dhr232 <dhruvilupadhyay@gmail.com>
2026-08-28 19:08:30 -0700

    Add static export config for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/ihealth-pharmacy-designs",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
